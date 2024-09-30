2ae597fa381e1f401548b3d2a9845a67d9c959b2
kellamchris <kellam.chris@gmail.com>
2024-09-30 17:23:31 -0600

    completed step 1

---
+++ README.txt
@@ -0,0 +1,1 @@
+Used ChatGPT for help in writing code for Step 1.
--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,31 @@
 // Build the metadata panel
 function buildMetadata(sample) {
+  console.log("buildMetadata function called"); // Check if the function is being called
+
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    console.log(data);
 
     // get the metadata field
-
+    let metadata = data.metadata;
+    console.log(metadata);
 
     // Filter the metadata for the object with the desired sample number
-
+    let result = metadata.filter(obj => obj.id == sample)[0];
+    console.log(result);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+    console.log("Metadata panel successfully updated");
 
   });
 }
@@ -26,27 +36,22 @@ function buildCharts(sample) {
 
     // Get the samples field
 
-
     // Filter the samples for the object with the desired sample number
-
 
     // Get the otu_ids, otu_labels, and sample_values
 
-
     // Build a Bubble Chart
-
 
     // Render the Bubble Chart
 
-
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
 
 
     // Render the Bar Chart
+    
 
   });
 }
@@ -57,9 +62,7 @@ function init() {
 
     // Get the names field
 
-
     // Use d3 to select the dropdown with id of `#selDataset`
-
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
@@ -68,16 +71,15 @@ function init() {
 
     // Get the first sample from the list
 
-
     // Build charts and metadata panel with the first sample
-
+    
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  
 }
 
 // Initialize the dashboard
